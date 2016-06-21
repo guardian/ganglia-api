cbe773d051168e05118774708ff7a0ce881617f4
Nick Satterly <nick.satterly@guardian.co.uk>
2016-06-21 10:03:54 +0100

    Make GANGLIA_PATH default to /etc/ganglia

--- ganglia/settings.py
+++ ganglia/settings.py
@@ -1,6 +1,6 @@
 DEBUG = True
 
-GANGLIA_PATH = '/usr/local/etc'  # where gmetad.conf is located
+GANGLIA_PATH = '/etc/ganglia'  # where gmetad.conf is located
 
 API_SERVER = 'http://ganglia-api.example.com:8080'  # where ganglia-api.py is hosted
 BASE_URL = '/ganglia/api/v2'
